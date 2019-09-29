acfae51bb69269a855c7c48b115d305136a66e5c
sfarri <seth.farr@icloud.com>
2019-09-28 23:57:27 -0600

    finished first assignment

---
+++ web/homepage.php
@@ -0,0 +1,11 @@
+<?php
+
+function randomPhrase() {
+    $number = random_int(0, 4);
+    $phrases = ["Remember that overconfidence is a slow and insidious killer.", "True desperation is known, only when escape is impossible.", "Curious is the trap-maker's art, his efficacy unwitnessed by his own eyes.", "Triumphant pride precipitates a dizzy fall.", "Victory. A hollow and ridiculous notion. We are born of this thing, made from it, and we will be returned to it in time."];
+    $phrase = $phrases[$number];
+    return $phrase;
+}
+
+    echo randomPhrase();
+?>
